c2ebf6a5372721fa4c08ea4e8673689ef53d9799
unknown <debbyria30@gmail.com>
2022-02-25 09:08:50 +0700

    nambah index.js

--- index.js
+++ index.js
@@ -48,3 +48,35 @@ forms.addEventListener("submit", function (e) {
 
 
   });
+
+  // CART2 JS
+  window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME'); */
+    
+    const name = sessionStorage.getItem('NAME');
+    const hp = sessionStorage.getItem('NOHP');
+    const ALAMAT = sessionStorage.getItem('ALAMAT');
+    const total = sessionStorage.getItem('TOTAL');
+    
+    document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-hp').innerHTML = hp;
+    document.getElementById('result-total').innerHTML = total;
+    document.getElementById('result-alamat').innerHTML = ALAMAT;
+})
+
+// CART JS
+  document.getElementById("plus-btn-incre-short").addEventListener("click", function(e) {
+    e.preventDefault()
+    let shortQty = document.getElementById("short-qty").value
+    console.log(shortQty)
+    shortQty+= 1
+    
+  })
